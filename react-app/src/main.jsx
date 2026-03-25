32d801a9874187117e3acf9b4f4f434cb5a0f11d
ricardddcode <ricardoreyengasado@gmail.com>
2026-03-25 13:37:45 -0600

    arreglo

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -6,10 +6,10 @@ import { Padre } from './components/Padre.jsx'
 import { Contador } from './components/Contador.jsx'
 import { Efecto } from './components/Efecto.jsx'
 
-//createRoot(document.getElementById('root')).render(
- // <StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <h1>Bienvenido a React</h1>
-    <Saludo nombre="Ricardo" edad={18} />
+    <Saludo nombre=" Josse Ricardo " edad={18} />
     <Padre />
     <Contador />
     <Efecto />
